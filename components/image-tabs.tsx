4481d2f50d1fcb308a1166c57f9514920c767fd5
Kalsoom Malik <mkalsoom548200@gmail.com>
2026-07-31 17:51:12 +0500

    Update image-tabs.tsx

--- components/image-tabs.tsx
+++ components/image-tabs.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import Image from "next/image";
-import { Button } from "@/components/ui/button"
+import { Button } from "../components/ui/button"
 import { Briefcase } from "lucide-react";
 
 export default function ImageTabs() {
